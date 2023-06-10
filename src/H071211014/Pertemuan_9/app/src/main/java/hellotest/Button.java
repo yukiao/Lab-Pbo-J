e1653dc74f6d9fcd05a297eac595d997283631a9
Attarmusharih <attar.musharih001@gmail.com>
2023-06-11 06:44:32 +0800

    kumpul tugas 9

---
+++ src/H071211014/Pertemuan_9/app/src/main/java/hellotest/Button.java
@@ -0,0 +1,11 @@
+package hellotest;
+
+public class Button {
+
+    public void setText(java.lang.String string) {
+    }
+
+    public void setOnAction(EventHandler<ActionEvent> eventHandler) {
+    }
+
+}
